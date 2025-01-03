4a001461493fe7936183484492a7125a0529d894
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2025-01-03 19:19:06 +0000

    aggiuntoFunzionalita

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book_club.db'
@@ -9,32 +11,100 @@ app.config['SECRET_KEY'] = 'chiave_segreta_sicura'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    books = db.relationship('Book', backref='user', lazy=True)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     author = db.Column(db.String(100), nullable=False)
     read = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref='comments')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
     books = Book.query.all()
     return render_template('index.html', books=books)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+        new_user = User(username=username, password=generate_password_hash(password))
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
 @app.route('/add', methods=['POST'])
+@login_required
 def add_book():
     title = request.form['title']
     author = request.form['author']
-    book = Book(title=title, author=author, read=False)
+    book = Book(title=title, author=author, user_id=current_user.id)
     db.session.add(book)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/mark_read/<int:book_id>')
+@login_required
+def mark_read(book_id):
+    book = Book.query.get_or_404(book_id)
+    if book.user_id == current_user.id:
+        book.read = True
+        db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/add_comment/<int:book_id>', methods=['POST'])
+@login_required
+def add_comment(book_id):
+    book = Book.query.get_or_404(book_id)
+    comment_text = request.form['comment']
+    comment = Comment(text=comment_text, book_id=book.id, user_id=current_user.id)
+    db.session.add(comment)
     db.session.commit()
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
